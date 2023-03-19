328c02f66b530cbded0f328a2fdbe970ce4e0713
Davide Garberi <dade.garberi@gmail.com>
2023-03-19 16:20:48 +0700

    sm6115-common: Rename rild.libpath property for treble compliance
    * rild.libpath is anyway being set to the same value as vendor.rild.libpath by ramdisk
    
    Change-Id: Ibecd22a9bb88125674c1d3e81bf4423e67083098

--- bengal.mk
+++ bengal.mk
@@ -861,8 +861,8 @@ PRODUCT_VENDOR_PROPERTIES += \
     persist.vendor.radio.procedure_bytes=SKIP \
     persist.vendor.radio.rat_on=combine \
     persist.vendor.radio.sib16_support=1 \
-    rild.libpath=/vendor/lib64/libril-qc-hal-qmi.so \
-    ro.com.android.dataroaming=true
+    ro.com.android.dataroaming=true \
+    vendor.rild.libpath=/vendor/lib64/libril-qc-hal-qmi.so
 
 # Sensors
 PRODUCT_PACKAGES += \
